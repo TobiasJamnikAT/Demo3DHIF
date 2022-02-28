cee08525aa4eafb62c2203bf893aa8172f664e11
hans <hans@softwaregaertner.at>
2022-02-28 11:07:34 +0100

    bibi

--- src/main/java/at/kaindorf/demo/HelloJohann.java
+++ src/main/java/at/kaindorf/demo/HelloJohann.java
@@ -3,5 +3,8 @@ package at.kaindorf.demo;
 public class HelloJohann {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("Sie nenne dich bibi, sie nenne dich bubu");
+        System.out.println("und sie wollen die bifi, direkt in die mum");
     }
 }
